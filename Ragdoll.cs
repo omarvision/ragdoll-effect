a09b56b55d12c4a74d75658cfa3f8d0e8457c15e
Omar Santiago <omarkenosia@gmail.com>
2021-07-12 17:41:55 -0400

    Add files via upload

---
+++ Ragdoll.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour
+{
+    private Rigidbody[] RigBones;
+
+    private void Start()
+    {
+        RigBones = this.gameObject.GetComponentsInChildren<Rigidbody>();
+        TurnRagdollOff();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            TurnRagdollOn();
+        }
+    }
+    private void TurnRagdollOff()
+    {
+        foreach (Rigidbody rb in RigBones)
+        {
+            rb.isKinematic = true;
+        }
+    }
+    private void TurnRagdollOn()
+    {
+        foreach (Rigidbody rb in RigBones)
+        {
+            rb.isKinematic = false;
+        }
+    }
+}
